03298c1c02b6d30201950ea32f27a71874df2eda
Nick Murphy <comfortablynick@gmail.com>
2019-03-03 21:23:34 -0600

    Add color to list output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-// use colored::*;
+use ansi_term::Color::Fixed;
 use env_logger;
 use log::info;
-use regex::Regex;
+use regex::{Captures, Regex};
 use std::io::prelude::{BufRead, Read};
 use std::{env, fs, io, path::PathBuf};
+
+const LIME: u8 = 154;
+const LIGHTORANGE: u8 = 215;
 
 struct Lines<R> {
     reader: io::BufReader<R>,
     buf: String,
+    length: u32,
 }
 
 impl<R: Read> Lines<R> {
@@ -15,6 +19,7 @@ impl<R: Read> Lines<R> {
         Lines {
             reader: io::BufReader::new(r),
             buf: String::new(),
+            length: 0,
         }
     }
     fn next<'a>(&'a mut self) -> Option<io::Result<&'a str>> {
@@ -25,6 +30,7 @@ impl<R: Read> Lines<R> {
                     None // no more lines!
                 } else {
                     let line = self.buf.trim_right();
+                    self.length += 1;
                     Some(Ok(line))
                 }
             }
@@ -37,8 +43,9 @@ fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "warning");
     env_logger::init();
 
-    let _re_date = Regex::new(r"(?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})").unwrap();
-    let _re_project = Regex::new(r"(?P<first>.*)(?P<project>\+.*)(?P<last>.*)").unwrap();
+    // let re_date = Regex::new(r"(?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})").unwrap();
+    let re_project = Regex::new(r"(\+\w+)").unwrap();
+    let re_context = Regex::new(r"(.*)(@\S+)(.*)").unwrap();
     let home = dirs::home_dir().expect("error getting home dir!");
     let mut path = PathBuf::from(home);
     path.push("Dropbox");
@@ -49,19 +56,24 @@ fn main() -> io::Result<()> {
     let file = fs::File::open(&path)?;
 
     let mut lines = Lines::new(file);
+    let mut ctr = 1;
+
     while let Some(line) = lines.next() {
         let line = line?;
-
-        // Example regex
-        // let caps = _re.captures(&line);
-        // match caps {
-        //     Some(c) => println!("Date found! {:?}", c),
-        //     None => (),
-        // }
-        // let line = _re_date.replace_all(&line, "$m/$d/$y");
-        let line = _re_project.replace_all(&line, "$first**$project**$last");
-
-        println!("{}", line);
+        let line = re_project.replace_all(&line, |c: &Captures| {
+            format!("{}", Fixed(LIME).paint(&c[0]))
+        });
+        let line = re_context.replace_all(&line, |caps: &Captures| {
+            format!(
+                "{}{}{}",
+                &caps[1],
+                Fixed(LIGHTORANGE).paint(&caps[2]),
+                &caps[3]
+            )
+        });
+        println!("{:02} {}", ctr, line);
+        ctr += 1;
     }
+    println!("--\nTODO: {} of {} tasks shown", ctr - 1, lines.length);
     Ok(())
 }
